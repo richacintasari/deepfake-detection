cd98111c690c083828d9f9775663dc1e8d3f158a
richacintasari <145654551+richacintasari@users.noreply.github.com>
2026-04-25 21:34:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -70,5 +70,6 @@ def index():
         error=error
     )
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
